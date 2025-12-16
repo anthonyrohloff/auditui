e5b9e9a016f80de53d7e966fc192e189942b9681
Anthony Rohloff <anthony.rohloff@gmail.com>
2025-12-15 20:05:55 -0500

    Added file extension filtering
    Implemented a method FilteredDirectoryTree that extends DirectoryTree.
    This function allows for a set to be passed as an argument to the
    yielding of the widget, returning only files with the extensions
    specified in the set.

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from textual.app import App, ComposeResult, on
 from textual.widgets import Header, Footer, DirectoryTree, Log
 from textual.containers import Horizontal
+from typing import Iterable
+from pathlib import Path
 
+class FilteredDirectoryTree(DirectoryTree):
+    def __init__(self, *args, allowed_extensions: set[str] | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.allowed_extensions = ({ext.lower() for ext in allowed_extensions} if allowed_extensions is not None else None)
+
+    def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
+        for path in paths:
+            if path.is_dir():
+                yield path
+            elif self.allowed_extensions is None:
+                yield path
+            elif path.suffix.lower() in self.allowed_extensions:
+                yield path
 
 class AudiTUI(App):
     CSS = """
@@ -24,13 +39,13 @@ class AudiTUI(App):
         yield Header()
 
         with Horizontal():
-            yield DirectoryTree("testdir", id="tree")
+            yield FilteredDirectoryTree("testdir", id="tree", allowed_extensions={".txt"})
             yield Log(id="log")
 
         yield Footer()
 
-    @on(DirectoryTree.FileSelected)
-    def file_selected(self, event: DirectoryTree.FileSelected) -> None:
+    @on(FilteredDirectoryTree.FileSelected)
+    def file_selected(self, event: FilteredDirectoryTree.FileSelected) -> None:
         log = self.query_one("#log", Log)
         log.clear()
 
@@ -43,7 +58,7 @@ class AudiTUI(App):
         self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
 
     def action_toggle_tree(self) -> None:
-        tree = self.query_one("#tree", DirectoryTree)
+        tree = self.query_one("#tree", FilteredDirectoryTree)
         tree.toggle_class("hidden")
 
 
